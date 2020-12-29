e69167ca2c99e8e000faf92a980f54f53632c241
Mauro Servienti <mauro.servienti@gmail.com>
2020-12-29 21:52:49 +0100

    basic list authors

---
+++ src/Eileen.Tests/When_listing_authors.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Eileen.Controllers;
+using Eileen.Data;
+using Eileen.Data.Views;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Eileen.Tests
+{
+    public class When_listing_authors : AbstractDatabaseTest
+    {
+        public When_listing_authors(ITestOutputHelper outputHelper) : base(outputHelper)
+        {
+        }
+        
+        [Fact]
+        public async Task With_default_paging_and_database_is_empty_list_is_empty()
+        {
+            var controller = new AuthorsController(CurrentDbContext);
+            var viewResult = await controller.List() as ViewResult;
+            var pagedResult = viewResult?.Model as PagedResults<AuthorsWithBooksCount>;
+            
+            Assert.NotNull(viewResult);
+            Assert.NotNull(pagedResult);
+            Assert.Empty(pagedResult.Results);
+        }
+    }
+}
