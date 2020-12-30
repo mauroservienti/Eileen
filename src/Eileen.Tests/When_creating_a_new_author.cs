29ae9f84c0a2f82e3c6ebf6b05e2bf01484ba098
mauroservienti <mauro.servienti@gmail.com>
2020-12-30 09:31:08 +0100

    New author test

---
+++ src/Eileen.Tests/When_creating_a_new_author.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Eileen.Controllers;
+using Eileen.Data;
+using Eileen.Data.Views;
+using Eileen.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Eileen.Tests
+{
+    public class When_creating_a_new_author : AbstractDatabaseTest
+    {
+        public When_creating_a_new_author(ITestOutputHelper outputHelper) : base(outputHelper)
+        {
+            CurrentDbContext.Database.Migrate();
+        }
+
+        [Fact]
+        public async Task Values_should_be_set_as_expected()
+        {
+            var newAuthorModel = new NewAuthorViewModel
+            {
+                Name = "Author name"
+            };
+
+            var controller = new AuthorsController(CurrentDbContext);
+            var redirectToActionResult = await controller.New(newAuthorModel) as RedirectToActionResult;
+            var newAuthorId = Convert.ToInt32(redirectToActionResult?.RouteValues["id"]);
+
+            var viewAuthorResult = await controller.View(newAuthorId) as ViewResult;
+            var author = viewAuthorResult?.Model as Author;
+
+            Assert.NotNull(author);
+            Assert.Equal(newAuthorModel.Name, author.Name);
+        }
+    }
+}
