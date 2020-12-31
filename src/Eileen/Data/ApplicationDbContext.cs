0aee58e597ad89d857a002713efcc0abdc90ac89
Mauro Servienti <mauro.servienti@gmail.com>
2020-12-31 16:16:04 +0100

    Automatically set CreatedOn and LastModifiedOn at SaveChanges invocation

--- src/Eileen/Data/ApplicationDbContext.cs
+++ src/Eileen/Data/ApplicationDbContext.cs
@@ -1,5 +1,11 @@
-﻿using Eileen.Data.Views;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Eileen.Data.Views;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace Eileen.Data
 {
@@ -13,7 +19,7 @@ namespace Eileen.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            
+
             modelBuilder
                 .Entity<AuthorsWithBooksCount>(eb =>
                 {
@@ -25,5 +31,40 @@ namespace Eileen.Data
         public DbSet<Book> Books { get; set; }
         public DbSet<Publisher> Publishers { get; set; }
         public DbSet<AuthorsWithBooksCount> AuthorsWithBooksCountView { get; set; }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            SetCreatedOnAndLastModifiedOnProperties(ChangeTracker.Entries(), NowGetter());
+
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess,
+            CancellationToken cancellationToken = default)
+        {
+            SetCreatedOnAndLastModifiedOnProperties(ChangeTracker.Entries(), NowGetter());
+
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        internal Func<DateTimeOffset> NowGetter {get; set;} = () => DateTimeOffset.Now;
+
+        static void SetCreatedOnAndLastModifiedOnProperties(IEnumerable<EntityEntry> entityEntries, DateTimeOffset now)
+        {
+            var entities = entityEntries.Where(entry => entry.Entity is IEntity)
+                .Select(entry => ((IEntity)entry.Entity, entry.State));
+            foreach ((IEntity iEntity, EntityState state) in entities)
+            {
+                if (state == EntityState.Added)
+                {
+                    iEntity.CreatedOn = now;
+                }
+
+                if (state == EntityState.Added || state == EntityState.Modified)
+                {
+                    iEntity.LastModifiedOn = now;
+                }
+            }
+        }
     }
 }
