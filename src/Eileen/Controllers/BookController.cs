643fce734e2aa50a2dc4dcc0a9bfa4b0e6f5b143
Mauro Servienti <mauro.servienti@gmail.com>
2021-01-01 18:05:47 +0100

    Book details

---
+++ src/Eileen/Controllers/BookController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Eileen.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eileen.Controllers
+{
+    [Authorize, Route("Book")]
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public BookController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Index(int id)
+        {
+            var book = await _dbContext.Books
+                .Include(b => b.Author)
+                .Include(b => b.Publisher)
+                .SingleOrDefaultAsync(b => b.Id == id);
+
+            //TODO handle book not found
+            return View(book);
+        }
+    }
+}
