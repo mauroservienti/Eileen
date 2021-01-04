76344520fe33a828bfb618d6eae2dfee61d865aa
Mauro Servienti <mauro.servienti@gmail.com>
2021-01-04 20:57:16 +0100

    Move pagination UI into a dedicated ViewComponent

---
+++ src/Eileen/Components/PagedResultsNavigationViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Eileen.Components
+{
+    [ViewComponent(Name = "Eileen.PagedResultsNavigation")]
+    public class PagedResultsNavigationViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(dynamic viewModel)
+        {
+            return View(viewModel);
+        }   
+    }
+}
